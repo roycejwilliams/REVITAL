c15c930f53580b69829c673415e6270b52020964
roycejwilliams <roycewilliamsj@gmail.com>
2023-06-27 22:49:11 -0500

    update

--- js/app.js
+++ js/app.js
@@ -108,25 +108,7 @@ changeSlide();
 
 
 /*on Scroll*/
-// const animation_elements = document.querySelectorAll('.animate-on-scroll');
 
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.add('animate');
-//       } else {
-//         entry.target.classList.remove('animate');
-//       }
-//     })
-// }, {
-//   threshold:0.5
-// });
-
-// for (let i = 0; i < animation_elements.length; i++) {
-//   const el = animation_elements[i];
-
-//   observer.observe(el);
-// }
 
 function animateOnScroll() {
   var animatedElements = document.querySelectorAll('.animate-element');
