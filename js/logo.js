63bf914bc78cf741d715221c64a7f3799dd3de1e
roycejwilliams <roycewilliamsj@gmail.com>
2023-08-19 10:08:12 -0500

    update

--- js/logo.js
+++ js/logo.js
@@ -1,18 +1,17 @@
 /*slide*/
 
-var index = 0;
-const imgs = document.getElementsByClassName('mySlides');
+let index = 0;
+const imgs = document.getElementsByClassName("mySlides");
 
 function changeBanner() {
-  [].forEach.call(imgs, function(v, i) {
+  [].forEach.call(imgs, function (v, i) {
     imgs[i].hidden = i !== index;
   });
   index = (index + 1) % imgs.length;
 }
 
-window.onload = function() {
+window.onload = function () {
   setInterval(changeBanner, 500);
 };
 
-
-// Us 
+// Us
